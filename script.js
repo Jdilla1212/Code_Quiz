6f5175904c2e68a5a7fc4bbbad6e72a2f235a6e5
Josh Padilla <jdp237@nau.edu>
2020-09-07 18:59:54 -0700

    added array of objects to function as my questions/answers

--- script.js
+++ script.js
@@ -1,18 +1,54 @@
 const timeEl = document.querySelector(".time");
+const question = document.querySelector(".question");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const startButton = document.querySelector("#start");
+const resetButton = document.querySelector("#reset");
+const highScoreButton = document.querySelector("#highscore");
 
 let secondsLeft = 60;
 
+
+//timer function
 function setTime() {
-  let timerInterval = setInterval(function() {
+  let timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds left";
 
-    if(secondsLeft === 0) {
+    if (secondsLeft === 0) {
       clearInterval(timerInterval);
       sendMessage();
     }
 
   }, 1000);
-}
+};
 
-setTime();
+
+let questionAnswerArray = [{
+    question: "q1",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "correct answer"
+  },
+  {
+    question: "q2",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "correct answer"
+  },
+  {
+    question: "q3",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "correct answer"
+  },
+  {
+    question: "q4",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "correct answer"
+  },
+  {
+    question: "q5",
+    answers: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    correctAnswer: "correct answer"
+  }
+];
