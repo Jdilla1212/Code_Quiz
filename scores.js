026e94a4ca4719154e5d9acc37d607f84e2bf20d
Josh Padilla <jdp237@nau.edu>
2020-09-09 22:30:03 -0700

    Clear button works now

--- scores.js
+++ scores.js
@@ -5,22 +5,24 @@ clearButton = document.querySelector("#clearbutton");
 
 //Function to append new scores to the leader board
 function appendHighScores() {
-    // .sort to sort thru high scores
     for (let i = 0; i < highScores.length; i++) {
         let liNode = document.createElement("li");
         liNode.setAttribute("class", "list-group-item")
         liNode.textContent = "Player: " + highScores[i].init + " score: " + highScores[i].score;
-        console.log(liNode)
         highScoreList.appendChild(liNode);
-
+    
     }
 
 };
+//Clear button calls clearhighscores function when clicked
+clearButton.addEventListener("click", function(event) {
+    clearHighscores();
+});
 
-// clearButton.addEventListener("click", function (event) {
-//     event.preventDefault;
-//     let highScoreList = [];
-
-// }
-
-appendHighScores();
+//Function to clear the high scores
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  };
+ 
+appendHighScores()
